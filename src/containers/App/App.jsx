593e1ab7f8996c9b03dea26b645190536cfd83b4
ElderSam <eldersamuel98@gmail.com>
2020-06-11 16:58:42 -0300

    add Routes

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -3,28 +3,22 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
-//import Routes from '../../routes';
+import Routes from '../../routes';
 
-//import { store, persistor } from '../../store';
-
-import { Authorize } from './../../containers';
+import { store, persistor } from '../../store';
 
 import './App.scss';
 
 const App = () => (
-    /*<Provider store={store}>
+    <Provider store={store}>
         <PersistGate persistor={persistor}>
-        <div className="app">
+        <div className="app" data-testid="app">
             <Router>
                 <Routes />
             </Router>
         </div>
         </PersistGate>
-    </Provider>*/
-
-    <div className="app">         
-        <Authorize />
-    </div>
+    </Provider>
 );
 
 export default App;
