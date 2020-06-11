e43e8e4abd123fa881a99697b89cfa64cda2e9c3
ElderSam <eldersamuel98@gmail.com>
2020-06-11 09:57:30 -0300

    show welcome message and add RouteHeader

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { RouteHeader, WelcomeBox } from './../../components';
 
 import './App.scss';
 
-const App = () => (<div className="app" />);
+const App = () => (
+    <div className="app">
+        <h1>Hello World!</h1>
+        <WelcomeBox name={'Samuel'}/>
+        {/*<RouteHeader />*/}
+
+    </div>
+);
 
 export default App;
